5c4bea157f144f53167b5940474f5d75fcf998a0
Moritz Pollack <moritze.pollack@gmail.com>
2023-04-20 16:15:33 +0000

    docs(licence): add LICENSE
    See: GPL-3.0 <https://choosealicense.com/licenses/gpl-3.0/>
    See: Issue 2 <https://github.com/unendlicherPing/LenneSchule/issues/2>

--- app/src/main/java/de/ping/lenneschule/presentation/screen/schedule/components/SubstitutionCard.kt
+++ app/src/main/java/de/ping/lenneschule/presentation/screen/schedule/components/SubstitutionCard.kt
@@ -1,3 +1,19 @@
+/*
+    Copyright (C) 2023  Moritz Pollack
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 package de.ping.lenneschule.presentation.screen.schedule.components
 
 import androidx.compose.animation.animateContentSize
@@ -48,7 +64,8 @@ fun SubstitutionCard(
         Column(
             Modifier
                 .fillMaxWidth()
-                .padding(12.dp)) {
+                .padding(12.dp)
+        ) {
             Row(modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween) {
                 Text(
                     text = "Class ${substitution.`class`}",
